74a06c765516faf470f9ec6bb1c5d82ac6bb6143
Wiam EL Khammal <162513652+WiamELKhammal@users.noreply.github.com>
2025-03-14 16:15:48 +0000

    logo change

--- UM6PCONNECT-FRONT/src/App.js
+++ UM6PCONNECT-FRONT/src/App.js
@@ -8,9 +8,13 @@ import SignUpPage from "./pages/SignUpPage";
 import SignInPage from "./pages/SignInPage"; // Import SignInPage
 import { UserProvider } from "./context/UserContext";
 import ChangeYourTempPass from "./pages/ChangeYourTempPass"; 
+import ProgramsParent from "./pages/Programs/ProgramsParent";
 
-import ProfilePage from "./components/Profile/ProfilePage";
-
+import ProfilePage from "./pages/Profile/ProfilePage";
+import ProfileContainer from "./pages/Profile/ProfileContainer";
+import HomePage from "./pages/Home/HomePage";
+import UserProfile from "./pages/Home/Userprofile/Userprofile";
+import UserList from "./components/UserList";
 const Layout = () => {
   const location = useLocation(); // Get current route
 
@@ -20,13 +24,17 @@ const Layout = () => {
       {!["/signup", "/signin", "/ChangeYourTempPass"].includes(location.pathname) && <Navbar />}
 
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/LandingPage2" element={<LandingPage2 />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/ChangeYourTempPass" element={<ChangeYourTempPass />} />
         <Route path="/signin" element={<SignInPage />} />
         <Route path="/ProfilePage" element={<ProfilePage />} />
+        <Route path="/LandingPage2" element={<LandingPage2 />} />
+        <Route path="/programs" element={<ProgramsParent />} />
+        <Route path="/profile" element={<ProfileContainer />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Userprofile/:userId" element={<UserProfile />} />
 
+        <Route path="/profile/:id" element={<UserProfile />} />
       </Routes>
     </>
   );
