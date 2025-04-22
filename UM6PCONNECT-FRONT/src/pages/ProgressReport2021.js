9c0e986838aae24d348c6388ec636bf93d223937
Wiam EL Khammal <162513652+WiamELKhammal@users.noreply.github.com>
2025-04-22 17:59:51 +0100

    HOME

--- UM6PCONNECT-FRONT/src/pages/ProgressReport2021.js
+++ UM6PCONNECT-FRONT/src/pages/ProgressReport2021.js
@@ -3,6 +3,7 @@ import ResidencesInfrastructure from './ResidencesInfrastructure';
 import TitleSection from './TitleSection';
 import AcademicLife from './AcademicLife';
 import AcademicLife2 from './AcademicLife2';
+import AcademicLife3 from './AcademicLife3';
 
 import AcademicGraphPage from './AcademicGraphPage';
 import AcademicVideoPage from './AcademicVideoPage';
@@ -17,7 +18,7 @@ const ProgressReport2021 = () => (
         <ProgressHero />
 
 
-        <TitleSection title="Academic Life & Attractiveness" id="academic" number="01" />
+        <TitleSection title="Academic Life & Attractiveness" id="academic"  />
 
         {/* ✅ AcademicLife with bottom border */}
         <Box sx={{ borderBottom: "1px solid #fff" }}>
@@ -26,52 +27,20 @@ const ProgressReport2021 = () => (
         <Box sx={{ borderBottom: "1px solid #fff" }}>
             <AcademicLife2 />
         </Box>
-
-
-        {/* ✅ AcademicGraphPage with bottom border */}
-        <Box sx={{ borderBottom: "1px solid #fff" }}>
+               {/* ✅ AcademicGraphPage with bottom border */}
+               <Box sx={{ borderBottom: "1px solid #fff" }}>
             <AcademicGraphPage />
         </Box>
-
-        <AcademicVideoPage />
-
-        <TitleSection
-            title="Residences & Infrastructure"
-            id="residences"
-            number="02"
-            borderTop
+        <TitleSection title="BLABLA" id="dining"  borderTop
         />
-        {/* Component for Residences goes here */}
-        <ResidencesInfrastructure />
-        <TitleSection title="Dining & Lifestyle" id="dining" number="03" borderTop
-        />
-
         <Box sx={{ borderBottom: "1px solid #fff" }}>
-            <Dinningandlifestyle />
+            <AcademicLife3 />
         </Box>
 
-        <Box sx={{ borderBottom: "1px solid #fff" }}>
-            <AcademicGraphPage />
-        </Box>
-
-        <TitleSection title="Wellness & Sports" id="wellness" number="04" />
-
-        <Box sx={{ borderBottom: "1px solid #fff" }}>
-            <Wellness />
-        </Box>
-        <AcademicVideoPage />
-
-        <TitleSection title="Staff & Family Services" id="staff" number="05" borderTop
-        />
-
-        <Box sx={{ borderBottom: "1px solid #fff" }}>
-            <Staffandfamilyservices />
-        </Box>
+ 
 
 
-        <Box sx={{ borderBottom: "1px solid #fff" }}>
-            <AcademicGraphPage />
-        </Box>
+
 
 
     </div>
